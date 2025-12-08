1abe469927fa465107c70a7441878da590a5fc13
Alex4386 <psh010209@gmail.com>
2025-12-08 15:29:47 +0900

    chore: fix all jobs properly

--- web/src/pages/admin/AdminJobs.tsx
+++ web/src/pages/admin/AdminJobs.tsx
@@ -132,7 +132,7 @@ export default function AdminJobs() {
         </div>
       ) : viewMode === 'grid' ? (
         <>
-          <div className="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+          <div className="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
             {data?.jobs.map((job: AdminJob) => {
               const StatusIcon = statusIcons[job.status]
               return (
